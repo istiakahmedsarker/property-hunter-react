caf79f204696ec8d00e26ec5e0473c49ba211445
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-01-21 20:46:29 +0600

    home page added

---
+++ property-hunter-react/src/Pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from '../../Components/Banner/Banner'
+import Testimonials from '../../Components/Testimonials/Testimonials'
+
+const Home = () => {
+    return (
+        <div>
+            <Banner/>
+            <Testimonials/>
+        </div>
+    );
+};
+
+export default Home;
