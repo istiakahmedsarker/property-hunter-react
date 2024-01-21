528b01f570f573d625d7ce16bf09217b25147e29
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-01-21 20:50:07 +0600

    blogs page updated and fetched data using useEffect

---
+++ property-hunter-react/src/Pages/Blogs/Blogs.jsx
@@ -0,0 +1,46 @@
+import BlogCard from '@/Components/BlogCard/BlogCard';
+import React, { useEffect, useState } from 'react';
+
+const Blogs = () => {
+    const [blogs, setBlogs] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await fetch('https://property-hunter-server.vercel.app/api/v1/blogs');
+                const blogsData = await res.json();
+                setBlogs(blogsData.data.blogs);
+            } catch (error) {
+                setError(error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []); // Empty dependency array ensures this effect runs once when the component mounts
+
+    return (
+        <div>
+            {loading && <p>Loading...</p>}
+            {error && <p>Error: {error.message}</p>}
+
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 ml-36">
+                <div className="col-span-2">
+                    <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 '>
+                        {
+                            blogs.map((blog) => <BlogCard key={blog._id} blog={blog} />)
+                        }
+                    </div>
+                </div>
+                <div className="">
+                    Featured Blogs
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Blogs;
