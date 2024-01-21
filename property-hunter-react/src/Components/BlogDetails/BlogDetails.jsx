6b7c87e2e431dcb4e0a6474e2b91f322bcfeec7c
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-01-21 21:16:51 +0600

    Update BlogDetails.jsx

--- property-hunter-react/src/Components/BlogDetails/BlogDetails.jsx
+++ property-hunter-react/src/Components/BlogDetails/BlogDetails.jsx
@@ -1,20 +1,22 @@
-import Image from "next/image";
 import React from "react";
-import BlogComments from "../BlogComments/page";
+import BlogComments from "../BlogComments/BlogComments";
+import { useParams } from "react-router-dom";
 
-const BlogDetails = ({blogs}) => {
+const BlogDetails = () => {
+  const {id} = useParams()
   const {heading,shortDescription,description,images,comments:comment} = blogs;
+  console.log(id)
   return (
     <div className=" max-w-6xl mx-auto mt-10">
       <div className="grid grid-cols-2 gap-4">
         <div>
-          <Image
+          <img
             className="rounded-md"
             src={images}
             height="350"
             width="600"
             alt="blog details image"
-          ></Image>
+          ></img>
         </div>
         <div className="bg-slate-100 px-4 py-7 rounded-md">
           <h2 className="text-3xl font-bold">{heading}</h2>
