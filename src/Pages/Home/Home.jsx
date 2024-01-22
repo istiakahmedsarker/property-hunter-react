406354363e4ba6d3514fe286735ce43586ccd9ff
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-01-22 17:36:41 +0600

    comments template created

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../../Components/Banner/Banner'
 import Testimonials from '../../Components/Testimonials/Testimonials'
+import CommentTemplate from '../../Components/Comments/Comments';
 
 const Home = () => {
     return (
         <div>
             <Banner/>
             <Testimonials/>
+            <CommentTemplate/>
         </div>
     );
 };
