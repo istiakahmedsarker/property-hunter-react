7273618d200353678bd3bdaad720f07d3804594a
Ruma543 <ruma.afrin543@gmail.com>
2024-01-22 16:16:29 +0600

    update properties details component

--- property-hunter-react/src/Components/PropertiesDetails/PropertiesDetails.jsx
+++ property-hunter-react/src/Components/PropertiesDetails/PropertiesDetails.jsx
@@ -10,16 +10,13 @@ const PropertiesDetails = () => {
   const details = cardDetails?.data?.property || {};
   console.log(details);
   return (
-    // <div>
-    //   <h3>details</h3>
-    // </div>
-    <div>
-      <h3>{details.propertyTitle}</h3>
-      <div>
+    <div className="w-11/12 mx-auto">
+      <div className="w-1/3 my-3">
+        <h3 className="font-semibold text-xl">{details.propertyTitle}</h3>
         <h3>
           {details.location.city},{details.location.state}
         </h3>
-        <div className="grid grid-cols-3 gap-5">
+        <div className=" flex gap-5 ">
           <h3 className="flex items-center">
             <IoBedOutline />
             {details.bedroom} Bed
@@ -34,19 +31,90 @@ const PropertiesDetails = () => {
           </h3>
         </div>
       </div>
-      <div>{/* image */}</div>
       <div>
+        {/* image */}
+        {details.propertyImages && details.propertyImages.length > 0 && (
+          <div className="grid grid-cols-2">
+            <div>
+              <img
+                src={details.propertyImages[0]}
+                alt={details.propertyTitle}
+                className="w-11/12 mx-auto rounded-lg "
+              ></img>
+            </div>
+            <div className="grid grid-cols-1 space-y-4">
+              <img
+                src={details.propertyImages[1]}
+                alt={details.propertyTitle}
+                className="w-11/12 mx-auto h-56  rounded-lg "
+              />
+              <img
+                src={details.propertyImages[2]}
+                alt={details.propertyTitle}
+                className="w-11/12 mx-auto h-56 rounded-lg "
+              />
+            </div>
+          </div>
+        )}
+      </div>
+      <div>
+        <h3></h3>
+      </div>
+      <div></div>
+      <div className="my-4">
         {/* properties Description */}
+        <h3 className="text-xl font-semibold ">Properties Description</h3>
         <h3>{details.description}</h3>
       </div>
-      <div>
+      <div className="w-2/3 my-4">
         {/* properties details */}
-        <h3>Properties Details</h3>
+        <h3 className="text-xl font-semibold">Properties Details</h3>
+        <div className="grid grid-cols-2 gap-5">
+          <div>
+            <h3>
+              <span className="font-semibold">Price:</span> $ {details.price}
+            </h3>
+            {/* parking need to be conditional */}
+            <h3>
+              <span className="font-semibold">Garage Size:</span>
+              {details.parking.spaces}
+            </h3>
+            <h3>
+              <span className="font-semibold">Bedroom:</span>
+              {details.bedroom}
+            </h3>
+            <h3>
+              <span className="font-semibold">Bathroom:</span>
+              {details.bathroom}
+            </h3>
+          </div>
+          <div>
+            <h3>
+              <span className="font-semibold">property Size:</span>
+              {details.squareFootage} sq Ft
+            </h3>
+            <h3>
+              <span className="font-semibold">Year of Build: </span>
+              {details.yearBuilt} sq Ft
+            </h3>
+            <h3>
+              <span className="font-semibold">property Type: </span>
+              {details.propertyType}
+            </h3>
+            <h3>
+              <span className="font-semibold">property Status: </span>
+              {details.propertyStatus}
+            </h3>
+          </div>
+        </div>
       </div>
-      <div>
-        <h3>{details.easement}</h3>
+      <div className="my-4">
+        <h3 className="text-xl font-semibold">Properties Features</h3>
+        <li>{details.easement[0]}</li>
+        <li>{details.easement[1]}</li>
+        <li>{details.easement[2]}</li>
+        <li>{details.easement[3]}</li>
       </div>
-      <h3>details page</h3>
     </div>
   );
 };
