9d62433882694796f9c5a7ec54da039ceaec034d
raselsharif <raselsharif9@gmail.com>
2024-01-25 20:34:55 +0600

    added toast in login and registration page

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import { RouterProvider } from "react-router-dom";
 import router from "./Routes/Routes";
 import AuthProvider from "./Providers/AuthProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "react-hot-toast";
 // Create a client
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Toaster />
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <RouterProvider router={router} />
