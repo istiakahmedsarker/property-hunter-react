146cdc5b5c3808d7df8cdfa9319e1272bc50bae3
Ruma543 <ruma.afrin543@gmail.com>
2024-01-25 11:29:00 +0600

    [feature](mapImplement)map implement in property details page

--- src/Components/PropertiesDetails/PropertiesDetails.jsx
+++ src/Components/PropertiesDetails/PropertiesDetails.jsx
@@ -22,12 +22,12 @@ const PropertiesDetails = () => {
     setIsFormOpen(!isFormOpen);
   };
 
-  // useEffect(() => {
-  //   const ifameData = document.getElementById('iframeId');
-  //   const lat = details.location.latitude;
-  //   const lon = details.location.longitude;
-  //   ifameData.src = `https://maps.google.com/maps?q=${lat},${lon}&hl=es;&output=embed`;
-  // });
+  useEffect(() => {
+    const ifameData = document.getElementById('iframeId');
+    const lat = details.location.latitude;
+    const lon = details.location.longitude;
+    ifameData.src = `https://maps.google.com/maps?q=${lat},${lon}&hl=es;&output=embed`;
+  });
 
   console.log(details);
   return (
@@ -236,9 +236,14 @@ const PropertiesDetails = () => {
               </div>
             </div>
             {/* map section */}
-            {/* <div>
-              <iframe id="iframeId" height="400px" width="100%"></iframe>
-            </div> */}
+            <div className="rounded-lg my-4">
+              <iframe
+                className="rounded-lg "
+                id="iframeId"
+                height="400px"
+                width="100%"
+              ></iframe>
+            </div>
           </div>
         </div>
         <div className="w-1/3">
