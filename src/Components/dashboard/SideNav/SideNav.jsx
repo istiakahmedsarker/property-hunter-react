f2ffeb1ed4bb980033b7baa4219ca09c8a580d39
Rasel Sharif <raselsharif9@gmail.com>
2024-01-25 16:14:23 +0600

    added dashboard profile page

--- src/Components/dashboard/SideNav/SideNav.jsx
+++ src/Components/dashboard/SideNav/SideNav.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
 import { GrAnnounce } from "react-icons/gr";
 import { MdSpaceDashboard } from "react-icons/md";
+import { FaUser } from "react-icons/fa";
 const SideNav = () => {
   return (
     <div className="pt-20 px-4">
@@ -29,6 +30,19 @@ const SideNav = () => {
           <div className="flex items-center gap-2">
             <GrAnnounce />
             Announcement
+          </div>
+        </NavLink>
+        <NavLink
+          to={"/dashboard/profile"}
+          className={({ isActive }) =>
+            isActive
+              ? "py-2 bg-[#eb6753] text-white px-2 rounded-md"
+              : "py-2 bg-white px-2 rounded-md"
+          }
+        >
+          <div className="flex items-center gap-2">
+            <FaUser />
+            Profile
           </div>
         </NavLink>
       </ul>
