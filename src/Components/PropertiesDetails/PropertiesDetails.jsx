d8295d2438a4161d1d6e3601e9c0c6bb1b43c8f7
Ruma543 <ruma.afrin543@gmail.com>
2024-01-26 15:39:50 +0600

    [update](responsive)update responsiveness in property details page

--- src/Components/PropertiesDetails/PropertiesDetails.jsx
+++ src/Components/PropertiesDetails/PropertiesDetails.jsx
@@ -33,7 +33,7 @@ const PropertiesDetails = () => {
   console.log(details);
   return (
     <div className="w-11/12 mx-auto">
-      <div className="w-1/3 my-3">
+      <div className="lg:w-1/3 w-full my-3">
         <h3 className="font-semibold text-xl">{details.propertyTitle}</h3>
         <h3 className="flex gap-3">
           <span> {details.location.city},</span>
@@ -57,84 +57,101 @@ const PropertiesDetails = () => {
       <div>
         {/* image */}
         {details.propertyImages && details.propertyImages.length > 0 && (
-          <div className="grid grid-cols-2">
-            <div>
+          <div className="grid lg:grid-cols-2 md:grid-cols-1 grid-cols-1 space-y-4">
+            <div className="h-full">
               <img
                 src={details.propertyImages[0]}
                 alt={details.propertyTitle}
                 className="w-11/12 mx-auto h-full rounded-lg "
               ></img>
             </div>
-            <div className="grid grid-cols-1 space-y-4">
-              <img
-                src={details.propertyImages[1]}
-                alt={details.propertyTitle}
-                className="w-11/12 mx-auto h-56  rounded-lg "
-              />
-              <img
-                src={details.propertyImages[2]}
-                alt={details.propertyTitle}
-                className="w-11/12 mx-auto h-56 rounded-lg "
-              />
+            <div className="grid grid-cols-1 space-y-4 ">
+              <div className="">
+                <img
+                  src={details.propertyImages[1]}
+                  alt={details.propertyTitle}
+                  className="w-11/12 mx-auto h-56  rounded-lg "
+                />
+              </div>
+
+              <div>
+                <img
+                  src={details.propertyImages[2]}
+                  alt={details.propertyTitle}
+                  className="w-11/12 mx-auto h-56 rounded-lg "
+                />
+              </div>
             </div>
           </div>
         )}
       </div>
-      <div className="flex gap-5 ">
-        <div className="w-2/3">
+      <div className="flex lg:flex-row flex-col gap-5 ">
+        <div className="lg:w-2/3 w-full">
           {/* overview */}
           <div className="w-full my-6 rounded-lg shadow-lg px-7 py-6 ">
             <h3 className="text-xl font-semibold py-5">Overview</h3>
-            <div className="grid grid-cols-3 gap-6">
-              <div className="flex justify-center items-center gap-5">
-                <h3 className="border-2 border-gray-300 rounded-lg px-3 py-3">
-                  <IoBedOutline />
-                </h3>
+            <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-2 gap-6">
+              <div className="flex items-center gap-5">
+                <div>
+                  <h3 className="border-2 border-gray-300 rounded-lg px-3 py-3">
+                    <IoBedOutline />
+                  </h3>
+                </div>
                 <div>
                   <h3 className="font-semibold">Bedroom</h3>
                   <h3>{details.bedroom}</h3>
                 </div>
               </div>
-              <div className="flex justify-center items-center gap-5">
-                <h3 className="border-2 border-gray-300 rounded-lg px-3 py-3">
-                  <PiBathtub />
-                </h3>
+              <div className="flex  items-center gap-5">
+                <div>
+                  <h3 className="border-2 border-gray-300 rounded-lg px-3 py-3">
+                    <PiBathtub />
+                  </h3>
+                </div>
                 <div>
                   <h3 className="font-semibold">Bathroom</h3>
                   <h3>{details.bathroom}</h3>
                 </div>
               </div>
-              <div className="flex justify-center items-center gap-5">
-                <h3 className="border-2 border-gray-300 rounded-lg px-3 py-3">
-                  <IoCalendarClearOutline />
-                </h3>
+              <div className="flex  items-center gap-5">
+                <div>
+                  <h3 className="border-2 border-gray-300 rounded-lg px-3 py-3">
+                    <IoCalendarClearOutline />
+                  </h3>
+                </div>
                 <div>
                   <h3 className="font-semibold">Year of Build</h3>
                   <h3>{details.yearBuilt}</h3>
                 </div>
               </div>
-              <div className="flex justify-center items-center gap-5">
-                <h3 className="border-2 border-gray-300 rounded-lg px-3 py-3">
-                  <BiSolidCarGarage />
-                </h3>
+              <div className="flex  items-center gap-5">
+                <div>
+                  <h3 className="border-2 border-gray-300 rounded-lg px-3 py-3">
+                    <BiSolidCarGarage />
+                  </h3>
+                </div>
                 <div>
                   <h3 className="font-semibold">Garage</h3>
                   <h3>{details.parking.spaces} sq Ft</h3>
                 </div>
               </div>
-              <div className="flex justify-center items-center gap-5">
-                <h3 className="border-2 border-gray-300 rounded-lg px-3 py-3">
-                  <BiShapeSquare />
-                </h3>
+              <div className="flex  items-center gap-5">
+                <div>
+                  <h3 className="border-2 border-gray-300 rounded-lg px-3 py-3">
+                    <BiShapeSquare />
+                  </h3>
+                </div>
                 <div>
                   <h3 className="font-semibold">Square Ft</h3>
                   <h3>{details.squareFootage}</h3>
                 </div>
               </div>
-              <div className="flex justify-center items-center gap-5">
-                <h3 className="border-2 border-gray-300 rounded-lg px-3 py-3">
-                  <IoHomeOutline />
-                </h3>
+              <div className="flex  items-center gap-5">
+                <div>
+                  <h3 className="border-2 border-gray-300 rounded-lg px-3 py-3">
+                    <IoHomeOutline />
+                  </h3>
+                </div>
                 <div>
                   <h3 className="font-semibold">Property Type</h3>
                   <h3>{details.propertyType}</h3>
@@ -144,11 +161,11 @@ const PropertiesDetails = () => {
           </div>
           {/* properties Description */}
           <div className="w-full my-6 rounded-lg shadow-lg px-7 py-6 ">
-            <h3 className="text-xl font-semibold py-5">
+            <h3 className="text-xl  font-semibold py-5">
               Properties Description
             </h3>
             {/* see more implementation */}
-            <p>
+            <p className="">
               {showFullDescription
                 ? details.description
                 : `${details.description.slice(0, 200)}${
@@ -164,27 +181,43 @@ const PropertiesDetails = () => {
             <div className="">
               {/* properties details */}
               <h3 className="text-xl font-semibold py-5">Properties Details</h3>
-              <div className="grid grid-cols-2 gap-5">
-                <div>
-                  <h3 className="grid grid-cols-2 py-2">
-                    <span className="font-semibold">Price</span>
-                    <span>: $ {details.price}</span>
-                  </h3>
-                  {/* parking need to be conditional */}
-                  <h3 className="grid grid-cols-2 py-2">
-                    <span className="font-semibold">Garage Size</span>
-                    <span>: {details.parking.spaces}</span>
-                  </h3>
-                  <h3 className="grid grid-cols-2 py-2">
-                    <span className="font-semibold">Bedroom</span>
-                    <span>: {details.bedroom}</span>
-                  </h3>
-                  <h3 className="grid grid-cols-2 py-2">
-                    <span className="font-semibold">Bathroom</span>
-                    <span>: {details.bathroom}</span>
-                  </h3>
-                </div>
-                <div>
+              <div className="grid grid-cols-2  lg:gap-5 gap-2">
+                <h3 className="grid grid-cols-2 py-1">
+                  <span className="font-semibold">Price</span>
+                  <span>: $ {details.price}</span>
+                </h3>
+                {/* parking need to be conditional */}
+                <h3 className="grid grid-cols-2 py-1">
+                  <span className="font-semibold">Garage Size</span>
+                  <span>: {details.parking.spaces}</span>
+                </h3>
+                <h3 className="grid grid-cols-2 py-1">
+                  <span className="font-semibold">Bedroom</span>
+                  <span>: {details.bedroom}</span>
+                </h3>
+                <h3 className="grid grid-cols-2 py-1">
+                  <span className="font-semibold">Bathroom</span>
+                  <span>: {details.bathroom}</span>
+                </h3>
+                {/*  */}
+                <h3 className="grid grid-cols-2 py-1">
+                  <span className="font-semibold">property Size</span>
+                  <span>: {details.squareFootage} sq Ft</span>
+                </h3>
+                <h3 className="grid grid-cols-2 py-1">
+                  <span className="font-semibold">Year of Build </span>
+                  <span>: {details.yearBuilt} </span>
+                </h3>
+                <h3 className="grid grid-cols-2 py-1">
+                  <span className="font-semibold">property Type </span>
+                  <span>: {details.propertyType}</span>
+                </h3>
+                <h3 className="grid grid-cols-2 py-1">
+                  <span className="font-semibold">property Status </span>
+                  <span>: {details.propertyStatus}</span>
+                </h3>
+
+                {/* <div>
                   <h3 className="grid grid-cols-2 py-2">
                     <span className="font-semibold">property Size</span>
                     <span>: {details.squareFootage} sq Ft</span>
@@ -201,7 +234,7 @@ const PropertiesDetails = () => {
                     <span className="font-semibold">property Status </span>
                     <span>: {details.propertyStatus}</span>
                   </h3>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
@@ -222,10 +255,10 @@ const PropertiesDetails = () => {
           {/* properties Address */}
           <div className=" w-full my-6 rounded-lg shadow-lg px-7 py-6">
             <h3 className="text-xl font-semibold py-5">Address</h3>
-            <div className="grid grid-cols-2 gap-5">
+            <div className="grid lg:grid-cols-2 grid-cols-1 gap-5">
               <div>
                 <h3 className="grid grid-cols-2 py-2">
-                  <span> Address</span>{' '}
+                  <span> Address</span>
                   <span>: {details.location.address}</span>
                 </h3>
                 <h3 className="grid grid-cols-2 py-2">
@@ -247,7 +280,7 @@ const PropertiesDetails = () => {
             </div>
           </div>
         </div>
-        <div className="w-1/3">
+        <div className="lg:w-1/3 w-full">
           {/* owner information */}
           <div className="w-full my-6 rounded-lg shadow-lg px-7 py-6 ">
             <h3 className="text-xl font-semibold py-5">Owner address</h3>
@@ -273,15 +306,13 @@ const PropertiesDetails = () => {
                 Essential information Submit
               </h3>
               <button
-                className="bg-blue-500 text-white px-4 py-2 rounded"
+                className="bg-[#eb6753] text-white px-4 py-2 rounded"
                 onClick={toggleForm}
               >
                 {isFormOpen ? 'Close Form' : 'Open Form'}
               </button>
 
-              {isFormOpen && (
-                <BuyerInquiryForm/>
-              )}
+              {isFormOpen && <BuyerInquiryForm />}
             </div>
           </div>
         </div>
