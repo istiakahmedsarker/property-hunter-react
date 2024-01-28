04ffac89a0c36a21f16ba47a286a1b157c3865d1
Ruma543 <ruma.afrin543@gmail.com>
2024-01-28 22:24:35 +0600

    create user management route in dashboard

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,28 +1,29 @@
-import { createBrowserRouter } from "react-router-dom";
-import Main from "../Layout/Main";
-import Login from "../Pages/Login/Login";
-import Home from "../Pages/Home/Home";
-import Blogs from "../Pages/Blogs/Blogs";
+import { createBrowserRouter } from 'react-router-dom';
+import Main from '../Layout/Main';
+import Login from '../Pages/Login/Login';
+import Home from '../Pages/Home/Home';
+import Blogs from '../Pages/Blogs/Blogs';
 // import BlogCard from "../Components/BlogCard/BlogCard";
-import BlogDetails from "../Components/BlogDetails/BlogDetails";
-import Properties from "../Pages/Properties/Properties";
-import PropertiesDetails from "../Components/PropertiesDetails/PropertiesDetails";
-import Register from "../Pages/Register/Register";
-import Dashboard from "../Layout/Dashboard";
-import HomeDashboard from "../Pages/Dashboard/HomeDashboard/HomeDashboard";
-import AddProperties from "../Pages/AddProperties/AddProperties";
-import AllProperties from "../Pages/Dashboard/Moderator/Pages/All Properties/AllProperties";
-import BlogPost from "../Components/BlogPost/BlogPost";
-import Announcement from "../Pages/Dashboard/Announcement/Announcement";
-import Profile from "../Pages/Dashboard/Profile/Profile";
+import BlogDetails from '../Components/BlogDetails/BlogDetails';
+import Properties from '../Pages/Properties/Properties';
+import PropertiesDetails from '../Components/PropertiesDetails/PropertiesDetails';
+import Register from '../Pages/Register/Register';
+import Dashboard from '../Layout/Dashboard';
+import HomeDashboard from '../Pages/Dashboard/HomeDashboard/HomeDashboard';
+import AddProperties from '../Pages/AddProperties/AddProperties';
+import AllProperties from '../Pages/Dashboard/Moderator/Pages/All Properties/AllProperties';
+import BlogPost from '../Components/BlogPost/BlogPost';
+import Announcement from '../Pages/Dashboard/Announcement/Announcement';
+import Profile from '../Pages/Dashboard/Profile/Profile';
+import UserManagement from '../Pages/Dashboard/Admin/Pages/UserManagement';
 // import
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Main></Main>,
     children: [
       {
-        path: "/",
+        path: '/',
         element: <Home />,
       },
       // {
@@ -30,11 +31,11 @@ const router = createBrowserRouter([
       //     element: <Apartment></Apartment>
       // },
       {
-        path: "/blogs",
+        path: '/blogs',
         element: <Blogs />,
       },
       {
-        path: "/blogs/:id",
+        path: '/blogs/:id',
         element: <BlogDetails />,
         // loader: ({ params }) => {
         //   console.log(params.id);
@@ -43,12 +44,12 @@ const router = createBrowserRouter([
       },
 
       {
-        path: "/properties",
+        path: '/properties',
         element: <Properties></Properties>,
       },
-      { path: "/createBlog", element: <BlogPost /> },
+      { path: '/createBlog', element: <BlogPost /> },
       {
-        path: "/propertiesDetails/:id",
+        path: '/propertiesDetails/:id',
         element: <PropertiesDetails></PropertiesDetails>,
         loader: ({ params }) =>
           fetch(
@@ -56,38 +57,42 @@ const router = createBrowserRouter([
           ),
       },
       {
-        path: "/addProperties",
+        path: '/addProperties',
         element: <AddProperties />,
       },
       {
-        path: "/login",
+        path: '/login',
         element: <Login />,
       },
       {
-        path: "/register",
+        path: '/register',
         element: <Register />,
       },
     ],
   },
   {
     // dashboard routes
-    path: "/dashboard",
+    path: '/dashboard',
     element: <Dashboard />,
     children: [
       {
-        path: "/dashboard/home",
+        path: '/dashboard/home',
         element: <HomeDashboard />,
       },
       {
-        path: "/dashboard/announcement",
+        path: '/dashboard/announcement',
         element: <Announcement />,
       },
       {
-        path: "/dashboard/all-properties",
+        path: '/dashboard/all-properties',
         element: <AllProperties />,
       },
       {
-        path: "/dashboard/profile",
+        path: '/dashboard/userManagement',
+        element: <UserManagement />,
+      },
+      {
+        path: '/dashboard/profile',
         element: <Profile />,
       },
     ],
