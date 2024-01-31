d2b0ec30d19ecaa015519992b150e0b628e75def
Rasel Sharif <raselsharif9@gmail.com>
2024-01-31 13:30:27 +0600

    added scrollbar design

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,46 +1,47 @@
-import { createBrowserRouter } from 'react-router-dom';
-import Main from '../Layout/Main';
-import Login from '../Pages/Login/Login';
-import Home from '../Pages/Home/Home';
-import Blogs from '../Pages/Blogs/Blogs';
-import BlogDetails from '../Components/BlogDetails/BlogDetails';
-import Properties from '../Pages/Properties/Properties';
-import PropertiesDetails from '../Components/PropertiesDetails/PropertiesDetails';
-import Register from '../Pages/Register/Register';
-import Dashboard from '../Layout/Dashboard';
-import HomeDashboard from '../Pages/Dashboard/HomeDashboard/HomeDashboard';
-import AddProperties from '../Pages/AddProperties/AddProperties';
-import AllProperties from '../Pages/Dashboard/Moderator/Pages/All Properties/AllProperties';
-import BlogPost from '../Components/BlogPost/BlogPost';
-import Announcement from '../Pages/Dashboard/Announcement/Announcement';
-import Profile from '../Pages/Dashboard/Profile/Profile';
-import UserManagement from '../Pages/Dashboard/Admin/Pages/UserManagement';
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../Layout/Main";
+import Login from "../Pages/Login/Login";
+import Home from "../Pages/Home/Home";
+import Blogs from "../Pages/Blogs/Blogs";
+// import BlogDetails from '../Components/BlogDetails/BlogDetails';
+import Properties from "../Pages/Properties/Properties";
+import PropertiesDetails from "../Components/PropertiesDetails/PropertiesDetails";
+import Register from "../Pages/Register/Register";
+import Dashboard from "../Layout/Dashboard";
+import HomeDashboard from "../Pages/Dashboard/HomeDashboard/HomeDashboard";
+import AddProperties from "../Pages/AddProperties/AddProperties";
+import AllProperties from "../Pages/Dashboard/Moderator/Pages/All Properties/AllProperties";
+import BlogPost from "../Components/BlogPost/BlogPost";
+import Announcement from "../Pages/Dashboard/Announcement/Announcement";
+import Profile from "../Pages/Dashboard/Profile/Profile";
+import UserManagement from "../Pages/Dashboard/Admin/Pages/UserManagement";
+import BlogDetails from "../Pages/BlogDetails/BlogDetails";
 // import
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <Main></Main>,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Home />,
       },
       {
-        path: '/blogs',
+        path: "/blogs",
         element: <Blogs />,
       },
       {
-        path: '/blogs/:id',
+        path: "/blogs/:id",
         element: <BlogDetails />,
       },
 
       {
-        path: '/properties',
+        path: "/properties",
         element: <Properties></Properties>,
       },
-      { path: '/createBlog', element: <BlogPost /> },
+      { path: "/createBlog", element: <BlogPost /> },
       {
-        path: '/propertiesDetails/:id',
+        path: "/propertiesDetails/:id",
         element: <PropertiesDetails></PropertiesDetails>,
         loader: ({ params }) =>
           fetch(
@@ -48,42 +49,42 @@ const router = createBrowserRouter([
           ),
       },
       {
-        path: '/addProperties',
+        path: "/addProperties",
         element: <AddProperties />,
       },
       {
-        path: '/login',
+        path: "/login",
         element: <Login />,
       },
       {
-        path: '/register',
+        path: "/register",
         element: <Register />,
       },
     ],
   },
   {
     // dashboard routes
-    path: '/dashboard',
+    path: "/dashboard",
     element: <Dashboard />,
     children: [
       {
-        path: '/dashboard/home',
+        path: "/dashboard/home",
         element: <HomeDashboard />,
       },
       {
-        path: '/dashboard/announcement',
+        path: "/dashboard/announcement",
         element: <Announcement />,
       },
       {
-        path: '/dashboard/all-properties',
+        path: "/dashboard/all-properties",
         element: <AllProperties />,
       },
       {
-        path: '/dashboard/userManagement',
+        path: "/dashboard/userManagement",
         element: <UserManagement />,
       },
       {
-        path: '/dashboard/profile',
+        path: "/dashboard/profile",
         element: <Profile />,
       },
     ],
