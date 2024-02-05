b98c0842310c608d231508edc30b8c18dd9a9775
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-02-05 21:56:20 +0600

    reverted changes in properties page

--- src/Pages/Properties/Properties.jsx
+++ src/Pages/Properties/Properties.jsx
@@ -4,71 +4,56 @@ import PropertiesCard from '../../Components/PropertiesCard/PropertiesCard';
 import PropertiesCardList from '../../Components/PropertiesCard/PropertiesCardList';
 // import { IoArrowUpOutline } from 'react-icons/io5';
 import TopButton from '../../Components/Shared/TopButton/TopButton';
-import useDebounce from '../../Hooks/useDebounce';
+import './PropertiesStyle.css';
+import { FcNext, FcPrevious } from 'react-icons/fc';
 
-const Properties = () => {
-  const [selectedOption, setSelectedOption] = useState('');
-  const [activePage, setActivePage] = useState(1);
-  const [searchText, setSearchText] = useState('');
+const Properties = ({ initialCards = [] }) => {
+  const [searchQuery, setSearchQuery] = useState('');
   const [isGrid, setIsGrid] = useState(false);
-  const debouncedSearchValue = useDebounce(searchText, 800);
+  const [cards, setCards] = useState(initialCards);
+  // filter with listing status
+  const [showAll, setShowAll] = useState(true);
+  const [showBuy, setShowBuy] = useState(true);
+  const [showRent, setShowRent] = useState(true);
 
-  const limit = 6;
-  const [checkboxes, setCheckboxes] = useState({
-    all: true,
-    rent: false,
-    sale: false,
-  });
+  // Filter with property type
+  const [showAllPropertyTypes, setShowAllPropertyTypes] = useState(true);
+  // const [showHouse, setShowHouse] = useState(true);
+  const [showApartment, setShowApartment] = useState(true);
+  const [showBuilding, setShowBuilding] = useState(true);
+  // const [showOffice, setShowOffice] = useState(true);
+  // const cards = await getAllCard();
+  // for pagination
+  // const [page, setPage] = useState(1);
+  useEffect(() => {
+    const fetchCards = async () => {
+      const allCards = await getAllCard();
+      setCards(allCards);
+    };
+    fetchCards();
+  }, []);
+  // console.log(cards.data);
+  const propertiesCards = cards?.data?.properties || [];
+  const filteredCards = propertiesCards.filter(card => {
+    const lowerCaseTitle = card.propertyTitle.toLowerCase();
+    const lowerCaseSearchQuery = searchQuery.toLowerCase();
+    const isTitleMatch = lowerCaseTitle.includes(lowerCaseSearchQuery);
 
-  const [typecCheckboxes, setTypeCheckboxes] = useState({
-    all: true,
-    apartment: false,
-    office: false,
-    villa: false,
-  });
+    // Apply listing status filters
+    if (showAll) return isTitleMatch;
+    if (showBuy && card.propertyStatus === 'sale') return isTitleMatch;
+    if (showRent && card.propertyStatus === 'rent') return isTitleMatch;
 
-  const handleCheckboxChange = (checkboxName) => {
-    const updatedCheckboxes = {};
+    // Apply property type filters
+    const isPropertyTypeMatch =
+      showAllPropertyTypes ||
+      // (showHouse && card.propertyType === 'house')
 
-    for (let key in checkboxes) {
-      updatedCheckboxes[key] = key === checkboxName;
-    }
+      (showApartment && card.propertyType === 'apartment') ||
+      (showBuilding && card.propertyType === 'villa');
+    // ||(showOffice && card.propertyType === 'office');
 
-    setCheckboxes(updatedCheckboxes);
-  };
-
-  const handleTypeCheckboxChange = (checkboxName) => {
-    const updatedCheckboxes = {};
-
-    for (let key in typecCheckboxes) {
-      updatedCheckboxes[key] = key === checkboxName;
-    }
-
-    setTypeCheckboxes(updatedCheckboxes);
-  };
-
-  const checkedItem = Object.keys(checkboxes).find(
-    (checkbox) => checkboxes[checkbox]
-  );
-  const typeCheckedItem = Object.keys(typecCheckboxes).find(
-    (checkbox) => typecCheckboxes[checkbox]
-  );
-
-  const { data: propertiesData, isPending } = useGetData({
-    key: [
-      'properties',
-      checkedItem,
-      typeCheckedItem,
-      selectedOption,
-      limit,
-      activePage,
-      debouncedSearchValue,
-    ],
-    api: `/properties?propertyStatus=${
-      checkedItem === 'all' ? '' : checkedItem
-    }&propertyType=${
-      typeCheckedItem === 'all' ? '' : typeCheckedItem
-    }&sort=${selectedOption}&&page=${activePage}&limit=${limit}&title=${searchText}`,
+    return isTitleMatch && isPropertyTypeMatch;
   });
   // for scroll to top
   // const scrollToTop = () => {
@@ -94,14 +79,6 @@ const Properties = () => {
   //   }
   // };
   // console.log(filteredCards);
-
-  if (isPending) {
-    return (
-      <p className="h-[90vh] flex flex-col items-center justify-center text-center">
-        Loading...
-      </p>
-    );
-  }
   return (
     <div className="w-11/12 mx-auto">
       <div className="my-6">
@@ -120,96 +97,126 @@ const Properties = () => {
               className="text-black px-4 py-3 w-full"
             />
           </div>
+
+          {/* Listing status checkboxes */}
+          <div className="w-full rounded-lg shadow-lg drop-shadow-lg bg-white px-5 py-6 my-4 ">
+            <h3 className="text-xl font-semibold py-3">Listing Status</h3>
+            <div className="flex flex-col space-y-2">
+              <label
+              // className="custom-checkbox"
+              >
+                <input
+                  className="mr-2"
+                  type="checkbox"
+                  checked={showAll}
+                  onChange={() => setShowAll(!showAll)}
+                />
+                Show All
+              </label>
+              <label
+              // className="custom-checkbox"
+              >
+                <input
+                  className="mr-2"
+                  type="checkbox"
+                  checked={showBuy}
+                  onChange={() => setShowBuy(!showBuy)}
+                />
+                For Sale
+              </label>
+              <label
+              // className="custom-checkbox"
+              >
+                <input
+                  className="mr-2 px-1 py-1"
+                  type="checkbox"
+                  checked={showRent}
+                  onChange={() => setShowRent(!showRent)}
+                />
+                For Rent
+              </label>
+            </div>
+          </div>
+          {/* Property type checkboxes */}
+          <div className="w-full rounded-lg shadow-lg drop-shadow-lg bg-white  px-5 py-6 my-4">
+            <h3 className="text-xl font-semibold py-3">Property Type</h3>
+            <div className="flex flex-col space-y-2">
+              <label>
+                <input
+                  className="mr-2"
+                  type="checkbox"
+                  checked={showAllPropertyTypes}
+                  onChange={() => {
+                    setShowAllPropertyTypes(!showAllPropertyTypes);
+                    // setShowHouse(true);
+                    setShowApartment(true);
+                    setShowBuilding(true);
+                    // setShowOffice(true);
+                  }}
+                />
+                Show All
+              </label>
+
+              <label>
+                <input
+                  className="mr-2"
+                  type="checkbox"
+                  // type="text"
+                  checked={showApartment}
+                  onChange={() => setShowApartment(!showApartment)}
+                />
+                Apartment
+              </label>
+              <label>
+                <input
+                  className="mr-2"
+                  type="checkbox"
+                  checked={showBuilding}
+                  onChange={() => setShowBuilding(!showBuilding)}
+                />
+                Villa
+              </label>
+            </div>
+          </div>
         </div>
-
-        {!propertiesData?.properties?.length ? (
-          <div className="md:col-span-8 sm:w-[70%] mx-auto md:w-full">
-            <p className=" h-[70vh] flex-col flex items-center justify-center">
-              No more items available
-            </p>
-          </div>
-        ) : (
-          <div
-            className="col-span-8 flex flex-col gap-16
-        "
-          >
-            <div>
-              <div className="flex justify-between">
-                <h4 className="text-xl font-semibold">
-                  {/* Show for All Properties :{propertiesCards.length || 0} */}
-                </h4>
-                {!isGrid ? (
-                  <button
-                    onClick={() => setIsGrid(true)}
-                    className="text-[#eb6753] font-semibold"
-                  >
-                    List view
-                  </button>
-                ) : (
-                  <button
-                    onClick={() => setIsGrid(false)}
-                    className="text-[#eb6753] font-semibold"
-                  >
-                    Grid View
-                  </button>
-                )}
-              </div>
-              {!isGrid ? (
-                <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-5">
-                  {propertiesData?.properties?.map((card) => (
-                    <PropertiesCard key={card._id} card={card}></PropertiesCard>
-                  ))}
-                </div>
-              ) : (
-                <div className="grid lg:grid-cols-1 md:grid-cols-1 sm:grid-cols-1 gap-5  my-6">
-                  {propertiesData?.properties?.map((card) => (
-                    <PropertiesCardList
-                      key={card._id}
-                      card={card}
-                    ></PropertiesCardList>
-                  ))}
-                </div>
-              )}
-            </div>
-            <div className="flex items-center justify-center gap-5">
+        <div className="lg:col-span-9 md:col-span-8 col-span-12">
+          <div className="flex justify-between">
+            <h4 className="text-xl font-semibold">
+              Show for All Properties :{filteredCards.length || 0}
+            </h4>
+            {!isGrid ? (
               <button
-                className={`${
-                  activePage === 1
-                    ? 'disabled bg-stone-400 rounded-full opacity-50 cursor-not-allowed p-3'
-                    : 'bg-white p-3 shadow-md rounded-full'
-                }`}
-                onClick={previousPage}
+                onClick={() => setIsGrid(true)}
+                className="text-[#eb6753] font-semibold"
               >
-                <FaArrowLeft />
+                List view
               </button>
-
-              {pages.map((pageNo) => (
-                <button
-                  className={`${
-                    activePage === pageNo
-                      ? 'bg-[#EB6753] font-semibold text-white px-4 py-2 rounded-full'
-                      : 'px-4 py-2 rounded-full font-semibold bg-white shadow-md'
-                  } `}
-                  key={pageNo}
-                  onClick={() => setActivePage(pageNo)}
-                >
-                  {pageNo}
-                </button>
+            ) : (
+              <button
+                onClick={() => setIsGrid(false)}
+                className="text-[#eb6753] font-semibold"
+              >
+                Grid View
+              </button>
+            )}
+          </div>
+          {!isGrid ? (
+            <div className="grid lg:grid-cols-3 md:grid-cols-1 sm:grid-cols-1 gap-5 lg:px-5">
+              {filteredCards.map(card => (
+                <PropertiesCard key={card._id} card={card}></PropertiesCard>
               ))}
-
-              <button
-                className={`${
-                  activePage === totalPage
-                    ? 'disabled bg-stone-400 rounded-full opacity-50 cursor-not-allowed p-3'
-                    : 'bg-white p-3 shadow-md rounded-full'
-                }`}
-                onClick={nextPage}
-              >
-                <FaArrowRight />
-              </button>
             </div>
-          </div>
-        )}
+          ) : (
+            <div className="grid lg:grid-cols-1 md:grid-cols-1 sm:grid-cols-1 gap-5 lg:px-5 my-6">
+              {filteredCards.map(card => (
+                <PropertiesCardList
+                  key={card._id}
+                  card={card}
+                ></PropertiesCardList>
+              ))}
+            </div>
+          )}
+        </div>
       </div>
       {/* for pagination */}
       {/* <div className="flex justify-center items-center">
