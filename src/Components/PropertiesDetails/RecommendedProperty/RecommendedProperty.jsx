2c2acee845b46af579896d290608f594c5adda4a
Ruma543 <ruma.afrin543@gmail.com>
2024-02-06 23:23:50 +0600

    [feature](create)create recommended componenent in property details page

--- src/Components/PropertiesDetails/RecommendedProperty/RecommendedProperty.jsx
+++ src/Components/PropertiesDetails/RecommendedProperty/RecommendedProperty.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const RecommendedProperty = () => {
+const RecommendedProperty = ({ cardDetails }) => {
+  // console.log(cardDetails?.data?.property?.propertyType);
+  const type = cardDetails?.data?.property?.propertyType || '';
+  console.log(type);
+
   return (
     <div>
-      <h3>recommended property</h3>
+      <h3>recommended property {type}</h3>
     </div>
   );
 };
