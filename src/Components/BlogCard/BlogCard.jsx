8b83f47aa555963b20de47c78ea08a5ed7218427
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-02-10 11:06:56 +0600

    updated the blog cards

--- src/Components/BlogCard/BlogCard.jsx
+++ src/Components/BlogCard/BlogCard.jsx
@@ -1,30 +1,20 @@
 import { Link } from 'react-router-dom';
 
 const BlogCard = ({ blog }) => {
-  const { _id, images, heading, description } = blog;
+  const { _id, images, heading, description, } = blog;
 
   return (
-    <div className="flex flex-col gap-8  h-[540px]">
-      <img
-        className="min-h-[250px] object-cover rounded-md"
-        src={images[0]}
-        alt="blog image"
-      />
-      <div className="flex flex-col gap-6">
-        <h2 className="text-xl  font-bold lg:leading-8">{heading}</h2>
-        <div className="flex flex-col gap-6">
-          <p
-            dangerouslySetInnerHTML={{
-              __html: description.substring(0, 110),
-            }}
-            className="-mt-2 text-lg leading-6 text-stone-400 "
-          ></p>
-          <Link to={`/blogs/${_id}`}>
-            <button className="bg-[#EB6753] px-5 py-2 rounded-sm text-red-50 font-semibold ">
-              Read full blog
-            </button>
-          </Link>
-        </div>
+    <div className="bg-white cursor-pointer overflow-hidden shadow-[0_2px_10px_-3px_rgba(6,81,237,0.3)] relative top-0 hover:-top-2 transition-all duration-300 rounded-xl">
+      <img src={images[0]} alt="Blog Post" className="w-96 h-60 object-cover" />
+      <div className="p-6">
+        <h3 className="text-xl font-bold text-[#333]">{heading}</h3>
+        <hr className="my-6" />
+        <p className="text-gray-400 text-sm" dangerouslySetInnerHTML={{ __html: description.substring(0, 110) }}></p>
+        <Link to={`/blogs/${_id}`}>
+          <button className="bg-[#EB6753] px-5 py-2 rounded-sm text-red-50 font-semibold">
+            Read full blog
+          </button>
+        </Link>
       </div>
     </div>
   );
