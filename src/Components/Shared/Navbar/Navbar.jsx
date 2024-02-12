1b4628afbadc7ce689db95d0a9641378a55bcf9a
Ruma543 <ruma.afrin543@gmail.com>
2024-02-12 07:19:03 +0600

    [feature](theme)light and dark theme button in navber

--- src/Components/Shared/Navbar/Navbar.jsx
+++ src/Components/Shared/Navbar/Navbar.jsx
@@ -2,12 +2,30 @@ import { Link, Navigate, useLocation, useNavigate } from 'react-router-dom';
 import './navbar.css';
 import useAuth from '../../../Hooks/useAuth';
 import { RiLogoutBoxLine } from 'react-icons/ri';
+import { useEffect, useState } from 'react';
 
 const Navbar = () => {
   const { pathname } = useLocation();
   const navigate = useNavigate();
   const { user, logOut } = useAuth();
-
+  // state for theme implement
+  const [theme, setTheme] = useState(
+    localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light'
+  );
+  //use local host for theme
+  useEffect(() => {
+    localStorage.setItem('theme', theme);
+    const localTheme = localStorage.getItem('theme');
+    document.querySelector('html').setAttribute('data-theme', localTheme);
+  }, [theme]);
+  // handle theme toggle
+  const handleToggle = e => {
+    if (e.target.checked) {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  };
   const handleLogOut = () => {
     logOut();
     navigate('/login');
@@ -18,9 +36,9 @@ const Navbar = () => {
       <li className="nav-link relative">
         <Link
           className={` ${
-            pathname === "/" ? "text-[#eb6753] font-bold" : "text-gray-700"
+            pathname === '/' ? 'text-[#eb6753] font-bold' : 'text-gray-700'
           } no-underline font-semibold text-lg`}
-          to={"/"}
+          to={'/'}
         >
           Home
         </Link>
@@ -28,9 +46,11 @@ const Navbar = () => {
       <li className="nav-link relative dropdown dropdown-hover">
         <Link
           className={` ${
-            pathname === "/properties" ? "text-[#eb6753] font-bold" : "text-gray-700"
+            pathname === '/properties'
+              ? 'text-[#eb6753] font-bold'
+              : 'text-gray-700'
           } no-underline font-semibold text-lg`}
-          to={"/properties"}
+          to={'/properties'}
         >
           Properties
         </Link>
@@ -38,9 +58,9 @@ const Navbar = () => {
       <li className="nav-link relative">
         <Link
           className={` ${
-            pathname === "/blogs" ? "text-[#eb6753] font-bold" : "text-gray-700"
+            pathname === '/blogs' ? 'text-[#eb6753] font-bold' : 'text-gray-700'
           } no-underline font-semibold text-lg`}
-          to={"/blogs"}
+          to={'/blogs'}
         >
           Blogs
         </Link>
@@ -49,9 +69,11 @@ const Navbar = () => {
         <li className="nav-link relative">
           <Link
             className={` ${
-              pathname === "/dashboard" ? "text-[#eb6753] font-bold" : "text-gray-700"
+              pathname === '/dashboard'
+                ? 'text-[#eb6753] font-bold'
+                : 'text-gray-700'
             } no-underline font-semibold text-lg`}
-            to={"/dashboard"}
+            to={'/dashboard'}
           >
             Dashboard
           </Link>
@@ -60,11 +82,11 @@ const Navbar = () => {
       <li>
         <Link
           className={` ${
-            pathname === "/add-property"
-              ? "text-[#eb6753] font-bold"
-              : "inline-block md:hidden text-gray-700"
+            pathname === '/add-property'
+              ? 'text-[#eb6753] font-bold'
+              : 'inline-block md:hidden text-gray-700'
           } no-underline font-semibold text-lg`}
-          to={"/addProperties"}
+          to={'/addProperties'}
         >
           Add Property
         </Link>
@@ -105,9 +127,15 @@ const Navbar = () => {
             </ul>
           </div>
           <a className=" text-[18px] md:text-xl flex items-center gap-2">
-         <img  className="hidden md:inline mix-blend h-6 md:h-10 w-6 md:w-10 object-cover" src="https://i.ibb.co/CJrmqqw/Screenshot-2024-01-15-161908-removebg-preview.png" alt="" />
-      
-            <h3 className=" font-semibold md:font-medium text-[18px] md:text-xl text-gray-900">Property-Hunter</h3>
+            <img
+              className="hidden md:inline mix-blend h-6 md:h-10 w-6 md:w-10 object-cover"
+              src="https://i.ibb.co/CJrmqqw/Screenshot-2024-01-15-161908-removebg-preview.png"
+              alt=""
+            />
+
+            <h3 className=" font-semibold md:font-medium text-[18px] md:text-xl text-gray-900">
+              Property-Hunter
+            </h3>
           </a>
         </div>
         <div className="navbar-center hidden lg:flex">
@@ -116,6 +144,32 @@ const Navbar = () => {
           </ul>
         </div>
         <div className="navbar-end">
+          {/* for dark and white theme */}
+          <label className="swap swap-rotate">
+            <input
+              type="checkbox"
+              onChange={handleToggle}
+              checked={theme === 'light' ? false : true}
+            />
+
+            {/* sun icon */}
+            <svg
+              className="swap-on fill-current w-5 h-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+            </svg>
+
+            {/* moon icon */}
+            <svg
+              className="swap-off fill-current w-5 h-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+            </svg>
+          </label>
           {user && (
             <div className="dropdown dropdown-end">
               <div
