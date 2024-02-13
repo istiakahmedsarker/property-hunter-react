6332597c592aa84611f2684bc4a5e4ab0e53abf2
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-02-13 22:16:45 +0600

    implemented dark theme using context api

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,13 +2,32 @@ import { Outlet } from 'react-router-dom';
 import Navbar from '../Components/Shared/Navbar/Navbar';
 import Footer from '../Components/Shared/Footer/Footer';
 import TopButton from '../Components/Shared/TopButton/TopButton';
+import { ThemeProvider } from '../Providers/ThemeContext';
+import { useEffect, useState } from 'react';
 
 const Main = () => {
+  const [themeMode, setThemeMode] = useState('light');
+
+  const darkTheme = () => {
+    setThemeMode('dark');
+  };
+
+  const lightTheme = () => {
+    setThemeMode('light');
+  };
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove('dark', 'light');
+    document.querySelector('html').classList.add(themeMode);
+  }, [themeMode]);
+
   return (
     <div>
-      <Navbar />
-      <Outlet></Outlet>
-      <Footer />
+      <ThemeProvider value={{ themeMode, darkTheme, lightTheme }}>
+        <Navbar />
+        <Outlet></Outlet>
+        <Footer />
+      </ThemeProvider>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode:"class",
   theme: {
-    extend: {},
+    
+    extend: {
+      colors:{
+        'primary-light': "#1476ff",
+        'secondary-light':"#f3f5ff",
+        'in-light': "#f9faff",
+        'primary-dark': "#19181e",
+        'secondary-dark':"#cfa55b",
+        'in-dark': "#a8a8a9"
+      },
+    },
   },
   plugins: [require('daisyui')],
-  daisyui: {
-    themes: ['light', 'dark'],
-  },
 };
