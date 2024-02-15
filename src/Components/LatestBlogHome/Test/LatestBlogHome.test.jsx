d79b2ba4cf12b9d7148e4b5775d917fc2338bed4
Ruma543 <ruma.afrin543@gmail.com>
2024-02-16 00:58:08 +0600

    [feature](videoAudioCall)implement AudioVideoCall component

---
+++ src/Components/LatestBlogHome/Test/LatestBlogHome.test.jsx
@@ -0,0 +1,12 @@
+// import { expect, test } from 'vitest';
+import LatestBlogHome from '../LatestBlogHome';
+// import { render, screen } from '@testing-library/react';
+
+import { render, screen } from '@testing-library/react';
+import { expect, test } from 'vitest';
+
+test('latest blog testing', () => {
+  render(<LatestBlogHome />);
+  const text = screen.getByText('Our latest Blogs');
+  expect(text).toBeInTheDocument();
+});
---
+++ src/Features/AudioVideoCall/AudioVideoCall.jsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import { ZegoUIKitPrebuilt } from '@zegocloud/zego-uikit-prebuilt';
+import './AudioVideoCall.css';
+function randomID(len) {
+  let result = '';
+  if (result) return result;
+  var chars = '12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP',
+    maxPos = chars.length,
+    i;
+  len = len || 5;
+  for (i = 0; i < len; i++) {
+    result += chars.charAt(Math.floor(Math.random() * maxPos));
+  }
+  return result;
+}
+
+export function getUrlParams(url = window.location.href) {
+  let urlStr = url.split('?')[1];
+  return new URLSearchParams(urlStr);
+}
+
+export default function AudioVideoCall() {
+  const roomID = getUrlParams().get('roomID') || randomID(5);
+  let myMeeting = async element => {
+    // generate Kit Token
+    const appID = 246548448;
+    const serverSecret = '957fedf83193d398810ed8d73d268a7d';
+    const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
+      appID,
+      serverSecret,
+      roomID,
+      randomID(5),
+      randomID(5)
+    );
+
+    // Create instance object from Kit Token.
+    const zp = ZegoUIKitPrebuilt.create(kitToken);
+    // start the call
+    zp.joinRoom({
+      container: element,
+      sharedLinks: [
+        {
+          name: 'Copy Link',
+          url:
+            window.location.protocol +
+            '//' +
+            window.location.host +
+            window.location.pathname +
+            '?roomID=' +
+            roomID,
+        },
+      ],
+      scenario: {
+        mode: ZegoUIKitPrebuilt.GroupCall,
+        // To implement 1-on-1 calls, modify the parameter here to [ZegoUIKitPrebuilt.OneONoneCall].
+      },
+    });
+  };
+
+  return (
+    <div
+      className="myCallContainer"
+      ref={myMeeting}
+      style={{ width: '100vw', height: '100vh' }}
+    ></div>
+  );
+}
