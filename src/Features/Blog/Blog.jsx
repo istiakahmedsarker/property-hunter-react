169957874b3b098244a309821991373db16c2522
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-02-17 21:05:54 +0600

    [feat](blogs) hide the scrollbar for latestBlogs

--- src/Features/Blog/Blog.jsx
+++ src/Features/Blog/Blog.jsx
@@ -51,7 +51,7 @@ const Blog = () => {
 
   return (
     <div>
-      {error && <p>{error}</p>}
+      {/* {error && <p>{error}</p>} */}
 
       <div className="grid grid-cols-1 md:grid-cols-12 gap-6 lg:gap-10 px-4 xl:px-0 max-w-[1340px] mx-auto my-10 items-start">
         <div className="md:col-span-8 sm:w-full mx-auto md:w-full order-2 md:order-1">
@@ -115,7 +115,7 @@ const Blog = () => {
             <FiSearch className="absolute text-2xl text-stone-400 left-10 top-10" />
           </div>
 
-          <div className="flex flex-col gap-8 bg-white shadow-sm px-6 py-8 rounded-md md:sticky md:top-0 md:mt-10 max-h-[50vh] overflow-y-auto">
+          <div className="flex flex-col gap-8 bg-white shadow-sm px-6 py-8 rounded-md md:sticky md:top-0 md:mt-10 max-h-full ">
             <h5 className="font-bold text-lg -mb-2">Latest blogs</h5>
             {latestBlogsData?.data?.blogs?.map((blog) => (
               <LatestBlog key={blog._id} blog={blog} />
