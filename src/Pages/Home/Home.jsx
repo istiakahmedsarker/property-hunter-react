09569f44e37ddca9e3da67b81d0fdb1e256cb84b
IsaT <rakiib010@gmail.com>
2024-02-19 15:38:44 +0600

    add why choose us section in home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,19 +5,20 @@ import ExploreTypes from '../../Components/ExploreTypes/ExploreTypes';
 import LatestBlogHome from '../../Components/LatestBlogHome/LatestBlogHome';
 import ContactUs from '../../Components/Contract/ContactUs/ContactUs';
 import FeaturedProperties from '../../Components/FeaturedProperties/FeaturedProperties';
+import WhyChooseUs from '../../Components/WhyChooseUs/WhyChooseUs';
 
 const Home = () => {
-
   return (
-      <div className='max-w-[1340px] mx-auto px-4 dark:bg-primary-dark ' >
-        <Banner/>
-        <ExploreTypes></ExploreTypes>
-        <FeaturedProperties></FeaturedProperties>
-        <LatestBlogHome></LatestBlogHome>
-        <Testimonials />
-        <Chatbot />
-        <ContactUs />
-      </div >
+    <div className="max-w-7xl mx-auto px-4 dark:bg-primary-dark ">
+      <Banner />
+      <ExploreTypes></ExploreTypes>
+      <FeaturedProperties></FeaturedProperties>
+      <LatestBlogHome></LatestBlogHome>
+      <Testimonials />
+      <WhyChooseUs />
+      <Chatbot />
+      <ContactUs />
+    </div>
   );
 };
 
