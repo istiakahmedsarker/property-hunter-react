207f857e017cf89a2a6d11d2fd273c289e0bf544
Ruma543 <ruma.afrin543@gmail.com>
2024-02-19 20:27:11 +0600

    [feature](propertydetails)update image and overview section in property details page

--- src/Features/PropertiesDetails/PropertiesDetails.jsx
+++ src/Features/PropertiesDetails/PropertiesDetails.jsx
@@ -70,38 +70,154 @@ const PropertiesDetails = () => {
 
       {/* image */}
       {details.propertyImages && details.propertyImages.length > 0 && (
-        <div className="grid  mx-auto lg:grid-cols-2 gap-4 md:grid-cols-1 grid-cols-1">
-          <div className="h-full">
-            <img
-              src={details.propertyImages[0]}
-              alt={details.propertyTitle}
-              className=" mx-auto w-full  h-56 rounded-lg "
-            ></img>
-          </div>
-          <div className="grid lg:grid-cols-2 gap-4 grid-cols-1 ">
-            <div className="">
+        <div className="relative">
+          <div className="grid  mx-auto lg:grid-cols-3 gap-1 md:grid-cols-1 grid-cols-1">
+            <div className="h-full">
+              <img
+                src={details.propertyImages[0]}
+                alt={details.propertyTitle}
+                className=" mx-auto w-full  h-96 rounded-sm "
+              ></img>
+            </div>
+            <div className="h-full">
               <img
                 src={details.propertyImages[1]}
                 alt={details.propertyTitle}
-                className="w-full mx-auto h-56  rounded-lg "
+                className="w-full mx-auto h-96  rounded-sm "
               />
             </div>
 
-            <div>
+            <div className="h-full">
               <img
                 src={details.propertyImages[2]}
                 alt={details.propertyTitle}
-                className="w-full mx-auto h-56 rounded-lg "
+                className="w-full mx-auto h-96 rounded-sm "
               />
+            </div>
+          </div>
+          <div className="rounded-sm shadow-lg drop-shadow-lg  absolute bg-white -bottom-10  left-20 right-20 ">
+            <div className="grid px-6 py-5 lg:grid-cols-6 md:grid-cols-2 grid-cols-2 gap-6">
+              {details?.rooms?.bedRooms ? (
+                <div className="flex items-center gap-5">
+                  <div>
+                    <h3 className="border-2 border-gray-300 rounded-lg px-3 py-3">
+                      <IoBedOutline />
+                    </h3>
+                  </div>
+                  <div>
+                    <h3 className="font-semibold">Bedroom</h3>
+                    <h3>{details.bedroom}</h3>
+                  </div>
+                </div>
+              ) : (
+                ''
+              )}
+
+              {details.rooms?.officeRooms ? (
+                <div className="flex  items-center gap-5">
+                  <div>
+                    <h3 className="border-2 border-gray-300 rounded-lg px-3 py-3">
+                      <RiHomeOfficeFill />
+                    </h3>
+                  </div>
+                  <div>
+                    <h3 className="font-semibold">Office room</h3>
+                    <h3></h3>
+                  </div>
+                </div>
+              ) : (
+                ''
+              )}
+
+              {details.rooms?.conferenceRooms ? (
+                <div className="flex  items-center gap-5">
+                  <div>
+                    <h3 className="border-2 border-gray-300 rounded-lg px-3 py-3">
+                      <FaUsersViewfinder />
+                    </h3>
+                  </div>
+                  <div>
+                    <h3 className="font-semibold">Conference room</h3>
+                    <h3>{details.bathroom}</h3>
+                  </div>
+                </div>
+              ) : (
+                ''
+              )}
+
+              {details.rooms?.bathRooms ? (
+                <div className="flex  items-center gap-5">
+                  <div>
+                    <h3 className="border-2 border-gray-300 rounded-lg px-3 py-3">
+                      <PiBathtub />
+                    </h3>
+                  </div>
+                  <div>
+                    <h3 className="font-semibold">Bathroom</h3>
+                    <h3>{details.bathroom}</h3>
+                  </div>
+                </div>
+              ) : (
+                ''
+              )}
+
+              <div className="flex  items-center gap-5">
+                <div>
+                  <h3 className="border-2 border-gray-300 rounded-lg px-3 py-3">
+                    <IoCalendarClearOutline />
+                  </h3>
+                </div>
+                <div>
+                  <h3 className="font-semibold">Year of Build</h3>
+                  <h3>{details.yearBuilt}</h3>
+                </div>
+              </div>
+              {details.parking.included ? (
+                <div className="flex  items-center gap-5">
+                  <div>
+                    <h3 className="border-2 border-gray-300 rounded-lg px-3 py-3">
+                      <BiSolidCarGarage />
+                    </h3>
+                  </div>
+                  <div>
+                    <h3 className="font-semibold">Garage</h3>
+                  </div>
+                </div>
+              ) : (
+                ''
+              )}
+
+              <div className="flex  items-center gap-5">
+                <div>
+                  <h3 className="border-2 border-gray-300 rounded-lg px-3 py-3">
+                    <BiShapeSquare />
+                  </h3>
+                </div>
+                <div>
+                  <h3 className="font-semibold">Square Ft</h3>
+                  <h3>{details.squareFootage}</h3>
+                </div>
+              </div>
+              <div className="flex  items-center gap-5">
+                <div>
+                  <h3 className="border-2 border-gray-300 rounded-lg px-3 py-3">
+                    <IoHomeOutline />
+                  </h3>
+                </div>
+                <div>
+                  <h3 className="font-semibold">Type</h3>
+                  <h3>{details.propertyType}</h3>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       )}
 
-      <div className="flex lg:flex-row flex-col gap-5 ">
+      <div className="flex lg:flex-row flex-col gap-5 mt-10 ">
         <div className="lg:w-2/3 w-full">
           {/* overview section */}
-          <div className="w-full my-6 rounded-sm shadow-lg drop-shadow-lg bg-white px-7 py-6 ">
+          {/* <div className="w-full my-6 rounded-sm shadow-lg drop-shadow-lg bg-white px-7 py-6 ">
             <h3 className="text-xl font-semibold py-5">Overview</h3>
             <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-2 gap-6">
               {details?.rooms?.bedRooms ? (
@@ -217,7 +333,7 @@ const PropertiesDetails = () => {
                 </div>
               </div>
             </div>
-          </div>
+          </div> */}
           {/* properties Description section */}
           <div className="w-full my-6 rounded-sm shadow-lg drop-shadow-lg bg-white px-7 py-6 ">
             <h3 className="text-xl  font-semibold py-5">
