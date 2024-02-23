74de627bda441d228141c5ac3e23f84a68fbf651
Istiak Ahmed Sarker <gamerestiak99@gmail.com>
2024-02-23 21:16:19 +0600

    [feat](AgentCard) updated colors for the black theme

--- src/Components/Agents/Agents.jsx
+++ src/Components/Agents/Agents.jsx
@@ -25,10 +25,10 @@ const Agents = () => {
             <div className="font-[sans-serif] text-[#333]">
                 <div className="max-w-5xl mx-auto">
                     <div className="max-w-2xl mx-auto text-center">
-                        <h2 className="text-3xl font-extrabold">Meet our team</h2>
-                        <p className="text-sm mt-4 leading-relaxed">Veniam proident aute magna anim excepteur et ex consectetur velit ullamco veniam minim aute sit. Ullamco nisi enim ipsum irure laboris ad ut. Esse cupidatat deserunt magna aute.</p>
+                        <h2 className="text-3xl font-extrabold dark:text-white">Meet our team</h2>
+                        <p className="text-sm mt-4 leading-relaxed dark:text-white">Veniam proident aute magna anim excepteur et ex consectetur velit ullamco veniam minim aute sit. Ullamco nisi enim ipsum irure laboris ad ut. Esse cupidatat deserunt magna aute.</p>
                     </div>
-                    <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-x-8 gap-y-10 max-md:justify-center mt-12">
+                    <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-x-8 gap-y-10 max-md:justify-center mt-12 mx-2">
                         {agentsData?.map((agent, index) => (
                             <AgentCard key={index} agents={agent} />
                         ))}
