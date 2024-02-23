f91cddbf5a70feceaf373b3d9223518648723c74
raselsharif <raselsharif9@gmail.com>
2024-02-23 21:45:25 +0600

    fix after google login data do not saved

--- src/Components/GoogleLogin/GoogleLogin.jsx
+++ src/Components/GoogleLogin/GoogleLogin.jsx
@@ -2,14 +2,30 @@ import { FaGoogle } from "react-icons/fa";
 import useAuth from "../../Hooks/useAuth";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
+import useAxios from "../../Hooks/useAxios";
 
 const GoogleLogin = () => {
   const { googleLogin } = useAuth();
   const goTo = useNavigate();
+  const axios = useAxios();
   const handleGoogleLogin = () => {
     googleLogin()
-      .then(() => {
+      .then((res) => {
         toast.success("Logged in successful");
+        const userInfo = {
+          name: res?.user?.displayName,
+          email: res?.user?.email,
+          role: "user",
+          image: res?.user?.photoURL || "",
+        };
+        axios
+          .post("/users", userInfo)
+          .then((user) => {
+            // console.log(user);
+          })
+          .catch(() => {
+            console.log("email exist");
+          });
         goTo("/");
       })
       .catch(() => {
